7047a19daba5fec9c1f970ca257547ff6a32e9bd
Andrew Dolce <andrew.dolce@gmail.com>
2024-11-19 16:37:05 -0500

    Specify base path to work with gh-pages

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  basePath: "/react-tutorial",
   output: "export",  // <=== enables static exports
   reactStrictMode: true,
 };
